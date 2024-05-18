1c68fadb40eab14980ec19a7ebb37e5e7be88491
automationbytes <automationbytes@gmail.com>
2024-05-18 20:52:37 +0530

    Added

--- CucumberDemo/src/test/java/StepDefn/Steps.java
+++ CucumberDemo/src/test/java/StepDefn/Steps.java
@@ -1,38 +1,52 @@
-//package StepDefn;
-//
-//import io.cucumber.java.en.*;
-//import org.junit.Assert;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//
-//import java.time.Duration;
-//
-//public class Steps {
-//
-//    WebDriver driver;
-//    @Given("the user launches the application")
-//    public void the_user_launches_the_application() {
-//        driver = new ChromeDriver();
-//        driver.get("https://www.facebook.com/");
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-//    }
-//    @Then("the user verifies the facebook logo")
-//    public void the_user_verifies_the_facebook_logo() {
-//        Assert.assertTrue(driver.findElement(By.xpath("//img[@alt=\"Facebook\"]")).isDisplayed());
-//    }
-//    @When("the user enters the username in username webedit")
-//    public void the_user_enters_the_username_in_username_webedit() {
-//        driver.findElement(By.id("email")).sendKeys("John");
-//    }
-//    @When("the user enters the password in password webedit")
-//    public void the_user_enters_the_password_in_password_webedit() {
-//        driver.findElement(By.name("pass")).sendKeys("Helo");
-//    }
-//    @When("the user clicks on login button")
-//    public void the_user_clicks_on_login_button() {
-//        driver.findElement(By.name("login")).click();
-//    }
-//
-//}
+package StepDefn;
+
+import io.cucumber.java.en.*;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Steps {
+
+    WebDriver driver;
+    @Given("the user launches the application")
+    public void the_user_launches_the_application() {
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+    @Then("the user verifies the facebook logo")
+    public void the_user_verifies_the_facebook_logo() {
+        Assert.assertTrue(driver.findElement(By.xpath("//img[@alt=\"Facebook\"]")).isDisplayed());
+    }
+    @When("the user enters the username in username webedit")
+    public void the_user_enters_the_username_in_username_webedit() {
+        driver.findElement(By.id("email")).sendKeys("John");
+    }
+    @When("the user enters the password in password webedit")
+    public void the_user_enters_the_password_in_password_webedit() {
+        driver.findElement(By.name("pass")).sendKeys("Helo");
+    }
+    @When("the user clicks on login button")
+    public void the_user_clicks_on_login_button() {
+        driver.findElement(By.name("login")).click();
+    }
+
+
+
+    @When("the user enters the {string} in username webedit")
+    public void the_user_enters_the_in_username_webedit(String user) {
+        driver.findElement(By.id("email")).sendKeys(user);
+
+    }
+    @When("the user enters the {string} in password webedit")
+    public void the_user_enters_the_in_password_webedit(String pass) {
+        driver.findElement(By.name("pass")).sendKeys(pass);
+    }
+
+
+
+}
--- CucumberDemo/src/test/resources/Features/Facebook.feature
+++ CucumberDemo/src/test/resources/Features/Facebook.feature
@@ -7,3 +7,18 @@ Feature: Facebook Login
     When the user enters the username in username webedit
     And the user enters the password in password webedit
     And the user clicks on login button
+
+
+
+  Scenario Outline: Verify facebook login with <username>
+
+    Given the user launches the application
+    Then the user verifies the facebook logo
+    When the user enters the "<username>" in username webedit
+    And the user enters the "<password>" in password webedit
+    And the user clicks on login button
+    Examples:
+      | username | password |
+    |Tom       |admin     |
+    |Jerry     |pass      |
+    |Kohli     |virat     |
